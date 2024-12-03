09344e86d341be3ad3782d4c11072e66effd1558
Javier Aldeguer Copete <javier.aldeguer1@gmail.com>
2024-12-03 23:22:29 +0100

    Versión Javier

--- Main.java
+++ Main.java
@@ -5,10 +5,10 @@ public class Main {
         OperacionesMatem op = new OperacionesMatem();
 
         int suma = op.calcularSuma(3);
-        System.out.println("La suma calculada es: " + suma); // Cambiado por Christian
+        System.out.print("La suma calculada es: " + suma); // Cambiado por Javier
 
         int[] numeros = {3, 8, 1, 6, 4, 5};
         int maximo = op.encontrarMaximo(numeros);
-        System.out.println("El número máximo es: " + maximo); // Cambiado por Christian
+        System.out.print("El número máximo es: " + maximo); // Cambiado por Javier
     }
 }

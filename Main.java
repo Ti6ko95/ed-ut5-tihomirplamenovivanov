fbd4a1b8ebb67a9adfa5685003f40957c5cbd0da
Christian <christianrodesvidal@gmail.com>
2024-12-03 21:41:26 +0100

    AGregado un mensaje en Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,13 @@ import debugg.OperacionesMatem;
 
 public class Main {
     public static void main(String[] args) {
-        OperacionesMatem op=new OperacionesMatem();
-        int suma=op.calcularSuma(3);
-        int[] numeros = {3, 8, 1, 6, 4} ;
-        int maximo=op.encontrarMaximo(numeros);
+        OperacionesMatem op = new OperacionesMatem();
+
+        int suma = op.calcularSuma(3);
+        System.out.println("La suma calculada es: " + suma); // Cambiado por Christian
+
+        int[] numeros = {3, 8, 1, 6, 4, 5};
+        int maximo = op.encontrarMaximo(numeros);
+        System.out.println("El número máximo es: " + maximo); // Cambiado por Christian
     }
 }
